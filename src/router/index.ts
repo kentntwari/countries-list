c918dc70c6e86a3a10819435f87b2c29a374551d
Kent Ntwari <designerjehovah@gmail.com>
2024-03-06 12:12:53 -0500

    feat:configure routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+import HomeView from '@/views/HomeView.vue'
+import CountryView from '@/views/CountryView.vue'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: []
+  routes: [
+    {
+      path: '/',
+      component: HomeView
+    },
+    {
+      path: '/:country',
+      name: 'country',
+      component: CountryView
+    }
+  ]
 })
 
 export default router
